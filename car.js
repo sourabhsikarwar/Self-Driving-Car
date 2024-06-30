c1c47f84a52ee1fd6ee148770ec015d4163b801d
Sourabh Sikarwar <sourabh.sikarwar@indmoney.com>
2024-06-30 19:24:43 +0530

    Car moving login

---
+++ car.js
@@ -0,0 +1,68 @@
+class Car {
+  constructor(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.speed = 0;
+    this.angle = 0;
+    this.acceleration = 0.2;
+    this.maxSpeed = 3;
+    this.friction = 0.05;
+
+    this.controls = new Controls();
+  }
+  update() {
+    this.#move();
+  }
+  // Private method
+  #move() {
+    if (this.controls.forward) {
+      this.speed += this.acceleration;
+    }
+    if (this.controls.reverse) {
+      this.speed -= this.acceleration;
+    }
+    if (this.speed > this.maxSpeed) {
+      this.speed = this.maxSpeed;
+    }
+    // for reverse --> speed is currently negative, which is not possible | Todo: Improve physics
+    if (this.speed < -this.maxSpeed / 2) {
+      this.speed = -this.maxSpeed / 2;
+    }
+    if (this.speed > 0) {
+      this.speed -= this.friction;
+    }
+    if (this.speed < 0) {
+      this.speed += this.friction;
+    }
+    if (Math.abs(this.speed) < this.friction) {
+      this.speed = 0;
+    }
+    // handling reverse gear
+    if (this.speed != 0) {
+      const reverseGear = this.speed > 0 ? 1 : -1;
+
+      if (this.controls.left) {
+        this.angle += 0.03 * reverseGear;
+      }
+      if (this.controls.right) {
+        this.angle -= 0.03 * reverseGear;
+      }
+    }
+
+    this.x -= this.speed * Math.sin(this.angle);
+    this.y -= this.speed * Math.cos(this.angle);
+  }
+  draw(ctx) {
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(-this.angle);
+    ctx.beginPath();
+    ctx.rect(-this.width / 2, -this.height / 2, this.width, this.height);
+    ctx.fill();
+
+    ctx.restore();
+  }
+}
